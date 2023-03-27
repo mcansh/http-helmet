c4e5ab8398bcc6178d2f5df868516247aeab44b2
Logan McAnsh <logan@mcan.sh>
2023-03-27 16:18:34 -0400

    fix: upgrade-insecure-requests is a boolean (#6)
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-secure-headers/src/rules/content-security-policy.ts
+++ packages/remix-secure-headers/src/rules/content-security-policy.ts
@@ -1,40 +1,34 @@
-import type { LiteralUnion } from "type-fest";
 import { dashify, isQuoted } from "../utils.js";
 
-type KnownSecurityPolicies = LiteralUnion<
-  | "childSrc"
-  | "connectSrc"
-  | "defaultSrc"
-  | "fontSrc"
-  | "frameSrc"
-  | "imgSrc"
-  | "manifestSrc"
-  | "mediaSrc"
-  | "objectSrc"
-  | "prefetchSrc"
-  | "scriptSrc"
-  | "scriptSrcElem"
-  | "scriptSrcAttr"
-  | "styleSrc"
-  | "styleSrcElem"
-  | "styleSrcAttr"
-  | "workerSrc"
-  | "baseUri"
-  | "sandbox"
-  | "formAction"
-  | "frameAncestors"
-  | "navigateTo"
-  | "reportUri"
-  | "reportTo"
-  | "requireSriFor"
-  | "requireTrustedTypesFor"
-  | "trustedTypes"
-  | "upgradeInsecureRequests",
-  string
->;
-
 export type ContentSecurityPolicy = {
-  [key in KnownSecurityPolicies]?: Array<string>;
+  childSrc?: Array<string>;
+  connectSrc?: Array<string>;
+  defaultSrc?: Array<string>;
+  fontSrc?: Array<string>;
+  frameSrc?: Array<string>;
+  imgSrc?: Array<string>;
+  manifestSrc?: Array<string>;
+  mediaSrc?: Array<string>;
+  objectSrc?: Array<string>;
+  prefetchSrc?: Array<string>;
+  scriptSrc?: Array<string>;
+  scriptSrcElem?: Array<string>;
+  scriptSrcAttr?: Array<string>;
+  styleSrc?: Array<string>;
+  styleSrcElem?: Array<string>;
+  styleSrcAttr?: Array<string>;
+  workerSrc?: Array<string>;
+  baseUri?: Array<string>;
+  sandbox?: Array<string>;
+  formAction?: Array<string>;
+  frameAncestors?: Array<string>;
+  navigateTo?: Array<string>;
+  reportUri?: Array<string>;
+  reportTo?: Array<string>;
+  requireSriFor?: Array<string>;
+  requireTrustedTypesFor?: Array<string>;
+  trustedTypes?: Array<string>;
+  upgradeInsecureRequests?: boolean;
 };
 
 const reservedCSPKeywords = new Set([
@@ -47,39 +41,46 @@ const reservedCSPKeywords = new Set([
 export function createContentSecurityPolicy(
   settings: ContentSecurityPolicy
 ): string {
-  return Object.entries(settings)
-    .map(([key, values]) => {
-      if (!Array.isArray(values)) {
+  let { upgradeInsecureRequests, ...rest } = settings;
+  let policy: Array<string> = [];
+
+  if (settings.upgradeInsecureRequests) {
+    policy.push("upgrade-insecure-requests");
+  }
+
+  for (let [key, values] of Object.entries(rest)) {
+    if (!Array.isArray(values)) {
+      throw new Error(
+        `[createContentSecurityPolicy]: The value of the "${key}" must be array of strings.`
+      );
+    }
+
+    const allowedValuesSeen: Set<string> = new Set();
+
+    values.forEach((allowedValue) => {
+      if (typeof allowedValue !== "string") {
         throw new Error(
-          `[createContentSecurityPolicy]: The value of the "${key}" must be array of strings.`
+          `[createContentSecurityPolicy]: The value of the "${key}" contains a non-string, which is not supported.`
         );
       }
 
-      const allowedValuesSeen: Set<string> = new Set();
+      if (allowedValuesSeen.has(allowedValue)) {
+        throw new Error(
+          `[createContentSecurityPolicy]: The value of the "${key}" contains duplicates, which it shouldn't.`
+        );
+      }
 
-      values.forEach((allowedValue) => {
-        if (typeof allowedValue !== "string") {
-          throw new Error(
-            `[createContentSecurityPolicy]: The value of the "${key}" contains a non-string, which is not supported.`
-          );
-        }
+      if (reservedCSPKeywords.has(allowedValue) && !isQuoted(allowedValue)) {
+        throw new Error(
+          `[createContentSecurityPolicy]: reserved keyword ${allowedValue} must be quoted.`
+        );
+      }
 
-        if (allowedValuesSeen.has(allowedValue)) {
-          throw new Error(
-            `[createContentSecurityPolicy]: The value of the "${key}" contains duplicates, which it shouldn't.`
-          );
-        }
+      allowedValuesSeen.add(allowedValue);
+    });
 
-        if (reservedCSPKeywords.has(allowedValue) && !isQuoted(allowedValue)) {
-          throw new Error(
-            `[createContentSecurityPolicy]: reserved keyword ${allowedValue} must be quoted.`
-          );
-        }
+    policy.push(`${dashify(key)} ${values.filter(Boolean).join(" ")}`);
+  }
 
-        allowedValuesSeen.add(allowedValue);
-      });
-
-      return `${dashify(key)} ${values.filter(Boolean).join(" ")}`;
-    })
-    .join("; ");
+  return policy.join("; ");
 }
