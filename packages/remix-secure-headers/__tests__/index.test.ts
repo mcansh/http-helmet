3550e1142f32eb36cf5ba5eff1e42dbd1fbabcf1
Logan McAnsh <logan@mcan.sh>
2023-07-14 17:49:37 -0400

    feat: add support for `X-XSS-Protection`

--- packages/remix-secure-headers/__tests__/index.test.ts
+++ packages/remix-secure-headers/__tests__/index.test.ts
@@ -2,38 +2,36 @@ import { expect, it } from "vitest";
 import { createSecureHeaders } from "../src";
 
 it("generates a config", () => {
-  expect(
-    createSecureHeaders({
-      "Strict-Transport-Security": {
-        maxAge: 3600,
-        includeSubDomains: true,
-        preload: true,
-      },
-      "Content-Security-Policy": {
-        defaultSrc: ["'self'"],
-        upgradeInsecureRequests: true,
-      },
-      "Permissions-Policy": {
-        battery: [],
-        accelerometer: ["self"],
-        autoplay: ["https://example.com"],
-        camera: ["*"],
-        fullscreen: ["self", "https://example.com", "https://example.org"],
-      },
-    })
-  ).toMatchInlineSnapshot(`
-    Headers {
-      Symbol(query): [
-        "content-security-policy",
-        "upgrade-insecure-requests; default-src 'self'",
-        "permissions-policy",
-        "battery=(), accelerometer=(self), autoplay=(\\"https://example.com\\"), camera=*, fullscreen=(self \\"https://example.com\\" \\"https://example.org\\")",
-        "strict-transport-security",
-        "max-age=3600; includeSubDomains; preload",
-      ],
-      Symbol(context): null,
-    }
-  `);
+  let headers = createSecureHeaders({
+    "Strict-Transport-Security": {
+      maxAge: 63072000,
+      includeSubDomains: true,
+      preload: true,
+    },
+    "Content-Security-Policy": {
+      defaultSrc: ["'self'"],
+      upgradeInsecureRequests: true,
+    },
+    "Permissions-Policy": {
+      battery: [],
+      accelerometer: ["self"],
+      autoplay: ["https://example.com"],
+      camera: ["*"],
+      fullscreen: ["self", "https://example.com", "https://example.org"],
+    },
+    "X-XSS-Protection": "1; report=https://google.com",
+  });
+
+  expect(headers.get("Strict-Transport-Security")).toBe(
+    "max-age=63072000; includeSubDomains; preload"
+  );
+  expect(headers.get("Content-Security-Policy")).toBe(
+    "upgrade-insecure-requests; default-src 'self'"
+  );
+  expect(headers.get("Permissions-Policy")).toBe(
+    `battery=(), accelerometer=(self), autoplay=("https://example.com"), camera=*, fullscreen=(self "https://example.com" "https://example.org")`
+  );
+  expect(headers.get("X-XSS-Protection")).toBe("1; report=https://google.com");
 });
 
 it("throws an error if the value is reserved", () => {
--- packages/remix-secure-headers/src/index.ts
+++ packages/remix-secure-headers/src/index.ts
@@ -32,48 +32,54 @@ export type CrossOriginOpenerPolicy =
   | "unsafe-none"
   | "same-origin-allow-popups"
   | "same-origin";
+export type XSSProtection = "0" | "1" | "1; mode=block" | `1; report=${string}`;
 
 export type CreateSecureHeaders = {
   /**
-   * Content Security Policy (CSP) is an added layer of security that helps to detect and mitigate certain types of attacks, including Cross-Site Scripting (XSS) and data injection attacks. These attacks are used for everything from data theft, to site defacement, to malware distribution.
+   * @description Content Security Policy (CSP) is an added layer of security that helps to detect and mitigate certain types of attacks, including Cross-Site Scripting (XSS) and data injection attacks. These attacks are used for everything from data theft, to site defacement, to malware distribution.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
    */
   "Content-Security-Policy"?: ContentSecurityPolicy;
   /**
-   * The X-Frame-Options HTTP response header can be used to indicate whether or not a browser should be allowed to render a page in a `<frame>`, `<iframe>`, `<embed>` or `<object>`. Sites can use this to avoid click-jacking attacks, by ensuring that their content is not embedded into other sites.
+   * @description The X-Frame-Options HTTP response header can be used to indicate whether or not a browser should be allowed to render a page in a `<frame>`, `<iframe>`, `<embed>` or `<object>`. Sites can use this to avoid click-jacking attacks, by ensuring that their content is not embedded into other sites.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
    */
   "X-Frame-Options"?: FrameOptions;
   /**
-   * The HTTP Feature-Policy header provides a mechanism to allow and deny the use of browser features in its own frame, and in content within any <iframe> elements in the document.
+   * @description The HTTP Feature-Policy header provides a mechanism to allow and deny the use of browser features in its own frame, and in content within any <iframe> elements in the document.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
    */
   "Permissions-Policy"?: PermissionsPolicy;
   /**
-   * The Referrer-Policy HTTP header controls how much referrer information (sent with the Referer header) should be included with requests. Aside from the HTTP header, you can set this policy in HTML.
+   * @description The Referrer-Policy HTTP header controls how much referrer information (sent with the Referer header) should be included with requests. Aside from the HTTP header, you can set this policy in HTML.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
    */
   "Referrer-Policy"?: ReferrerPolicy;
   /**
-   * The HTTP Strict-Transport-Security response header (often abbreviated as HSTS) informs browsers that the site should only be accessed using HTTPS, and that any future attempts to access it using HTTP should automatically be converted to HTTPS.
+   * @description HTTP Strict-Transport-Security response header (often abbreviated as HSTS) informs browsers that the site should only be accessed using HTTPS, and that any future attempts to access it using HTTP should automatically be converted to HTTPS.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
    */
   "Strict-Transport-Security"?: StrictTransportSecurity;
   /**
-   * The X-DNS-Prefetch-Control HTTP response header controls DNS prefetching, a feature by which browsers proactively perform domain name resolution on both links that the user may choose to follow as well as URLs for items referenced by the document, including images, CSS, JavaScript, and so forth.
+   * @description The X-DNS-Prefetch-Control HTTP response header controls DNS prefetching, a feature by which browsers proactively perform domain name resolution on both links that the user may choose to follow as well as URLs for items referenced by the document, including images, CSS, JavaScript, and so forth.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
    */
   "X-DNS-Prefetch-Control"?: DNSPrefetchControl;
   /**
-   * The X-Content-Type-Options response HTTP header is a marker used by the server to indicate that the MIME types advertised in the Content-Type headers should be followed and not be changed. The header allows you to avoid MIME type sniffing by saying that the MIME types are deliberately configured.
+   * @description The X-Content-Type-Options response HTTP header is a marker used by the server to indicate that the MIME types advertised in the Content-Type headers should be followed and not be changed. The header allows you to avoid MIME type sniffing by saying that the MIME types are deliberately configured.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
    */
   "X-Content-Type-Options"?: ContentTypeOptions;
   /**
-   * The HTTP Cross-Origin-Opener-Policy (COOP) response header allows you to ensure a top-level document does not share a browsing context group with cross-origin documents.
+   * @description The HTTP Cross-Origin-Opener-Policy (COOP) response header allows you to ensure a top-level document does not share a browsing context group with cross-origin documents.
    * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Opener-Policy
    */
   "Cross-Origin-Opener-Policy"?: CrossOriginOpenerPolicy;
+  /**
+	  @description The HTTP X-XSS-Protection response header is a feature of Internet Explorer, Chrome and Safari that stops pages from loading when they detect reflected cross-site scripting (XSS) attacks. These protections are largely unnecessary in modern browsers when sites implement a strong Content-Security-Policy that disables the use of inline JavaScript ('unsafe-inline').
+		@see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+  */
+  "X-XSS-Protection"?: XSSProtection;
 };
 
 export function createSecureHeaders(options: CreateSecureHeaders) {
@@ -106,6 +112,10 @@ export function createSecureHeaders(options: CreateSecureHeaders) {
 
   if (options["Referrer-Policy"]) {
     headers.set("Referrer-Policy", options["Referrer-Policy"]);
+  }
+
+  if (options["X-XSS-Protection"]) {
+    headers.set("X-XSS-Protection", options["X-XSS-Protection"]);
   }
 
   return headers;
