badc8ba0fc4a5b9c7120332233de1df63b252152
Logan McAnsh <logan@mcan.sh>
2023-07-20 21:01:36 -0400

    chore(example): use v2 future flags
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-secure-headers/src/index.ts
+++ packages/remix-secure-headers/src/index.ts
@@ -5,59 +5,59 @@ console.warn(
 import * as helmet from "@mcansh/http-helmet";
 
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type ContentSecurityPolicy = helmet.ContentSecurityPolicy;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type ContentTypeOptions = helmet.ContentTypeOptions;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type CreateSecureHeaders = helmet.CreateSecureHeaders;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type CrossOriginOpenerPolicy = helmet.CrossOriginOpenerPolicy;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type DNSPrefetchControl = helmet.DNSPrefetchControl;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type FrameOptions = helmet.FrameOptions;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type PermissionsPolicy = helmet.PermissionsPolicy;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type ReferrerPolicy = helmet.ReferrerPolicy;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type StrictTransportSecurity = helmet.StrictTransportSecurity;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export type XSSProtection = helmet.XSSProtection;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export const createContentSecurityPolicy = helmet.createContentSecurityPolicy;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export const createPermissionsPolicy = helmet.createPermissionsPolicy;
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export const createSecureHeaders = helmet.createSecureHeaders;
 // prettier-ignore
 /**
- * @deprecated this package has been renamed to @mcansh/http-helmet and will be removed in a future release
+ * @deprecated this package has been renamed to `@mcansh/http-helmet` and will be removed in a future release
  */
 export const createStrictTransportSecurity = helmet.createStrictTransportSecurity;
