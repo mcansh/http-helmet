0e49b70f5bcec079dee257ed4a1d96058bce9c94
Logan McAnsh <logan@mcan.sh>
2023-07-20 21:06:47 -0400

    chore(example): update package import
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- example/app/entry.server.tsx
+++ example/app/entry.server.tsx
@@ -5,7 +5,7 @@ import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
-import { createSecureHeaders } from "@mcansh/remix-secure-headers";
+import { createSecureHeaders } from "@mcansh/http-helmet";
 import { NonceContext } from "./nonce";
 
 let ABORT_DELAY = 5000;
