03ebcb177b807b3d34795d5b421525af7070a640
Logan McAnsh <logan@mcan.sh>
2025-03-09 21:24:41 -0400

    feat: allow shorthand for Strict-Transport-Security
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/http-helmet/src/rules/strict-transport-security.ts
+++ packages/http-helmet/src/rules/strict-transport-security.ts
@@ -4,9 +4,17 @@ export type StrictTransportSecurity = {
   preload?: boolean;
 };
 
+export function createStrictTransportSecurity(options: true): string;
 export function createStrictTransportSecurity(
   options: StrictTransportSecurity,
+): string;
+export function createStrictTransportSecurity(
+  options: StrictTransportSecurity | true,
 ): string {
+  if (options === true) {
+    options = { maxAge: 15552000, includeSubDomains: true, preload: true };
+  }
+
   let header = `max-age=${options.maxAge}`;
 
   if (options.includeSubDomains) {
